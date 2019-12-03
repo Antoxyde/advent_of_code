f754794d6d42d8ef99ca45c8757f735014662cf3
Antoxyde <Antoxyde@protonmail.com>
2019-12-03 11:13:02 +0100

    added day 3

--- 2019/day_3/day_3_part2.rs
+++ 2019/day_3/day_3_part2.rs
@@ -6,8 +6,8 @@ use std::iter::FromIterator;
 fn gen_way(insts: &[&str]) -> Vec<(i32,i32)> {
     
     let mut way = Vec::new();
-
     let mut actual_point : (i32, i32) = (0,0);
+    //way.push(actual_point);
 
     for inst in insts {
 
@@ -16,23 +16,23 @@ fn gen_way(insts: &[&str]) -> Vec<(i32,i32)> {
 
         match dir {
             'R' => {
-                let mut pts: Vec<(i32,i32)> = (1..dist).map(|x| (actual_point.0 + x, actual_point.1)).collect();
+                let mut pts: Vec<(i32,i32)> = (1..dist + 1).map(|x| (actual_point.0 + x, actual_point.1)).collect();
                 way.append(&mut pts);
                 actual_point = (actual_point.0 + dist, actual_point.1);
             },
             'L' => {
-                let mut pts: Vec<(i32,i32)> = (1..dist).map(|x| (actual_point.0 - x, actual_point.1)).collect();
+                let mut pts: Vec<(i32,i32)> = (1..dist + 1).map(|x| (actual_point.0 - x, actual_point.1)).collect();
                 way.append(&mut pts);
                 actual_point = (actual_point.0 - dist, actual_point.1);
             },
             'U' => {
-                let mut pts: Vec<(i32,i32)> = (1..dist).map(|x| (actual_point.0 , actual_point.1 + x)).collect();
+                let mut pts: Vec<(i32,i32)> = (1..dist + 1).map(|x| (actual_point.0 , actual_point.1 + x)).collect();
                 way.append(&mut pts);
                 actual_point = (actual_point.0, actual_point.1 + dist);
             },
 
             'D' => {
-                let mut pts: Vec<(i32,i32)> = (1..dist).map(|x| (actual_point.0 , actual_point.1 - x)).collect();
+                let mut pts: Vec<(i32,i32)> = (1..dist + 1).map(|x| (actual_point.0 , actual_point.1 - x)).collect();
                 way.append(&mut pts);
                 actual_point = (actual_point.0, actual_point.1 - dist);
             },
@@ -50,14 +50,16 @@ fn main() -> std::io::Result<()> {
     let mut inp = String::new();
     file.read_to_string(&mut inp)?;
     let mut sets : Vec<HashSet<(i32, i32)>> = Vec::new();
+    let mut vecs : Vec<Vec<(i32,i32)>> = Vec::new();
     for way in inp.lines() {
 
         let insts : Vec<&str> = way.split(",").collect();
         let w = gen_way(&insts);
+        vecs.push(w.clone());
         sets.push(HashSet::from_iter(w.iter().cloned()));
     }
     
-    let result = sets[0].intersection(&sets[1]).map(|p| p.0.abs() + p.1.abs()).min();
+    let result = sets[0].intersection(&sets[1]).map(|p| vecs[0].iter().position(|r| p == r).unwrap() +  vecs[1].iter().position(|r| r == p).unwrap() + 2).min();
     
     println!("Result is {}", result.unwrap());
     Ok(())
