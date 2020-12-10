82d8908bfca2d85ff560a9966e901c2b81b9e778
antox <git@w0nderland.fr>
2020-12-10 17:30:54 +0100

    cleanup day09

--- 2020/day09/target/debug/day09.d
+++
@@ -1,1 +0,0 @@
-/home/antox/repos/advent_of_code/2020/day09/target/debug/day09: /home/antox/repos/advent_of_code/2020/day09/src/main.rs
--- 2020/day09/target/debug/deps/day09-9fd85421415faf55.d
+++
@@ -1,5 +0,0 @@
-/home/antox/repos/advent_of_code/2020/day09/target/debug/deps/day09-9fd85421415faf55: src/main.rs
-
-/home/antox/repos/advent_of_code/2020/day09/target/debug/deps/day09-9fd85421415faf55.d: src/main.rs
-
-src/main.rs:
