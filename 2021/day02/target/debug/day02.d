b1c30db2dae955cf7ddfb57ccfddbb89a2c9f4d8
antox <git@w0nderland.fr>
2021-12-02 12:09:07 +0100

    removed binfiles

--- 2021/day02/target/debug/day02.d
+++
@@ -1,1 +0,0 @@
-/home/antox/repos/advent_of_code/2021/day02/target/debug/day02: /home/antox/repos/advent_of_code/2021/day02/src/main.rs
--- 2021/day02/target/debug/deps/day02-c466a7f909b1c19a.d
+++
@@ -1,5 +0,0 @@
-/home/antox/repos/advent_of_code/2021/day02/target/debug/deps/day02-c466a7f909b1c19a: src/main.rs
-
-/home/antox/repos/advent_of_code/2021/day02/target/debug/deps/day02-c466a7f909b1c19a.d: src/main.rs
-
-src/main.rs:
